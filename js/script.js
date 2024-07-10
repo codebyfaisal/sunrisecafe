7b6ae0b6da994ca728cf1a2049ffdf39dd66c57d
Muhammad Faisal <90336526+codebyfaisal@users.noreply.github.com>
2024-07-10 09:35:20 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,121 @@
+// DOM elements
+const message = document.getElementById("message");
+const textarea = document.querySelector("#form textarea");
+const carouselItems = document.querySelectorAll("#home .carousel-item");
+const navbarToggler = document.querySelector('.navbar-toggler');
+const iconSpan = navbarToggler.querySelector('span');
+
+// Menu elements
+const menuImages = document.querySelectorAll("#menu .menu-item img");
+const menuNames = document.querySelectorAll("#menu .menu-item .menu-title");
+const menuLoader = document.getElementById("loader");
+let lastMenuItem = 0;
+
+// Function to hide menu items
+function menuHider(start) {
+  const activeTabPane = document.querySelector("#menu .tab-content .tab-pane.active");
+  const menuItems = activeTabPane.querySelectorAll("#menu .menu-items .col-lg-3");
+  lastMenuItem = start;
+  menuItems.forEach((menuItem, index) => {
+    if (index > 3) {
+      menuItem.style.display = "none";
+      lastMenuItem = 3;
+    } else {
+      menuItem.style.display = "block";
+    }
+  });
+}
+
+// Initial call to menuHider
+menuHider(0);
+
+// Event listeners for menu navigation
+document.querySelectorAll("#menu ul .nav-item .nav-link").forEach(element => {
+  element.addEventListener("click", () => {
+    menuHider(0);
+    menuLoader.style.display = "block";
+  });
+});
+
+// Event listener for menu loader
+menuLoader.addEventListener("click", () => {
+  const activeTabPane = document.querySelector("#menu .tab-content .tab-pane.active");
+  const menuItems = activeTabPane.querySelectorAll("#menu .menu-items .col-lg-3");
+  lastMenuItem = lastMenuItem + 5;
+  menuItems.forEach((menuItem, index) => {
+    if (index < lastMenuItem) {
+      menuItem.style.display = "block";
+    }
+  });
+
+  if (lastMenuItem >= menuItems.length) menuLoader.style.display = "none";
+});
+
+// Ensure menu image titles match images
+if (menuImages.length === menuNames.length) {
+  menuImages.forEach((img, index) => {
+    const src = img.getAttribute("src");
+    const fileName = src.split('/').pop();
+    const nameWithoutExtension = fileName.split('.')[ 0 ];
+    const menuItemName = nameWithoutExtension.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+
+    menuNames[ index ].innerHTML = menuItemName;
+  });
+} else {
+  console.error("The number of menu images and menu titles do not match.");
+}
+
+// Event listener for navbar toggler
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarCollapse = document.querySelector('#navbarNavAltMarkup');
+
+  navbarToggler.addEventListener('click', function () {
+    iconSpan.classList.toggle('la-times');
+  });
+
+  // Close navbar on outside click
+  document.addEventListener('click', function (event) {
+    const isClickInside = navbarToggler.contains(event.target) || navbarCollapse.contains(event.target);
+
+    if (!isClickInside && navbarCollapse.classList.contains('show')) {
+      navbarToggler.click();
+    }
+  });
+});
+
+// Style carousel items
+for (let i = 0; i < carouselItems.length; i++) {
+  carouselItems[ i ].style.height = "100vh";
+  carouselItems[ i ].style.backgroundImage = `url(../images/hero/hero-${i}.jpg)`;
+  carouselItems[ i ].style.backgroundColor = "rgba(0,0,0,0.8)";
+  carouselItems[ i ].style.backgroundRepeat = "no-repeat";
+  carouselItems[ i ].style.backgroundSize = "cover";
+  carouselItems[ i ].style.backgroundPosition = "center";
+}
+
+// Jump to top functionality
+document.addEventListener("DOMContentLoaded", function () {
+  let jumperArea = document.querySelector(".menu-items");
+  let jumpToTopButton = document.querySelector(".jump-to-top");
+
+  if (!jumperArea || !jumpToTopButton) {
+    console.error("Error: Element not found");
+    return;
+  }
+
+  window.addEventListener("scroll", function () {
+    let servicesPosition = jumperArea.getBoundingClientRect().top;
+    let windowHeight = window.innerHeight - 400;
+
+    if (servicesPosition < windowHeight) {
+      jumpToTopButton.style.cursor = "pointer";
+      jumpToTopButton.style.display = "block";
+    } else {
+      jumpToTopButton.style.display = "none";
+    }
+  });
+
+  jumpToTopButton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
